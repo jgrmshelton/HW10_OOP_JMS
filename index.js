a227b9434566aa07d68c45d3ed3b46f5f1c63074
Julia Shelton <87336948+jgrmshelton@users.noreply.github.com>
2021-10-06 21:00:33 -0500

    commit

--- index.js
+++ index.js
@@ -1,138 +1,149 @@
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Engineer");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer
-    .prompt([
-    {
-        type: "list",
-        choices: ["Employee", "Engineer", "Intern", "Manager"],
-        message: "What is your job title?",
-        name: "jobTitle",
-    },
-    {
-        type: "input",
-        message: "fullName",
-        name: "yourName",
-    },
-    ])
-    .then((response) => {
-        var squad = `<!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>DevSquad</title>
-            <link rel="stylesheet" type="text/css" href="style.css" />
-        </head>
-        
-    // beautify
-        <body style="margin: 0; padding: 0; background-color: pink;">
-            <header style="
-                        border: solid 2px plum;
-                        height: 200px;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        ">
-        
-                <h1>DevSquad</h1>
-            </header>
-            <section style="
-                        border: solid 2px plum;
-                        background: white;
-                        display: flex;
-                        flex-wrap: nowrap;
-                        ">
-                <div style="
-                            border: solid 2px plum;
-                            width: 25%;
-                            height: 425px;
-                            text-align: center;
-                            font-size: 1.5rem;
-                            ">
-                    Manager
-                    <br />
-                    <h5 style="text-align: left">
-                        <span>Name: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Email: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>ID: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Office Number: </span>
-                    </h5>
-                </div>
-                <div style="
-                        border: solid 2px plum;
-                        width: 25%;
-                        height: 425px;
-                        text-align: center;
-                        font-size: 1.5rem;
-                        ">
-                    Engineer
-                    <br />
-                    <h5 style="text-align: left">
-                        <span>Name: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Email: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>ID: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Office Number: </span>
-                </div>
-                <div style="
-                    border: solid 2px plum;
-                    width: 25%;
-                    height: 425px;
-                    text-align: center;
-                    font-size: 1.5rem;
-                    ">
-                    Employee
-                    <br />
-                    <h5 style="text-align: left">
-                        <span>Name: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Email: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>ID: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Office Number: </span>
-                </div>
-                <div style="
-                    border: solid 2px plum;
-                    width: 25%;
-                    height: 425px;
-                    text-align: center;
-                    font-size: 1.5rem;
-                    ">
-                    Intern
-                    <br />
-                    <h5 style="text-align: left">
-                        <span>Name: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Email: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>ID: </span>
-                    </h5>
-                    <h5 style="text-align: left">
-                        <span>Office Number: </span>
-                    </h5>
-                </div>
-            </section>
-        </body>
-        
-        </html>`;
+const outputDir = path.resolve(__dirname, "output");
+const outputPath = path.join(outputDir, "team.html");
+
+const render = require("./lib/pg-render");
+
+const teamQuestions = [
+  {
+    type: "input",
+    message: "fullName",
+    name: "yourName",
+  },
+  {
+    type: "list",
+    choices: ["Employee", "Engineer", "Intern", "Manager"],
+    message: "What is your job title?",
+    name: "jobTitle",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What the team member's id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the team member's email?",
+  },
+];
+
+const team = [];
+const generateTeam = () => {
+  inquirer
+    .prompt(teamQuestions)
+    .then((answers) => {
+      inquirer
+        .prompt([
+          {
+            when: () => answers.role === "Manager",
+            type: "input",
+            message: "What is their office number",
+            name: "officeNumber",
+          },
+          {
+            when: () => answers.role === "Engineer",
+
+            type: "input",
+            message: "Github Username:",
+            name: "github",
+          },
+
+          {
+            when: () => answers.role === "Employee",
+
+            type: "input",
+            message: "Github Username:",
+            name: "github",
+          },
+
+          {
+            when: () => answers.role === "Intern",
+
+            type: "input",
+            message: "School/University",
+            name: "school",
+          },
+
+          {
+            type: "confirm",
+            message: "Would you like to add another team member?",
+            name: "yes",
+          },
+        ])
+
+        .then((answers1) => {
+          if (answers1.role === "Manager") {
+            const manager = new Manager(
+              answers1.name,
+              answers1.id,
+              answers1.email,
+              answers1.role,
+              answers2.officeNumber
+            );
+            team.push(manager);
+          }
+
+          if (answers1.role === "Engineer") {
+            const engineer = new Engineer(
+              answers1.name,
+              answers1.id,
+              answers1.email,
+              answers1.role,
+              answers2.github
+            );
+            team.push(engineer);
+          }
+
+          if (answers1.role === "Employee") {
+            const engineer = new Employee(
+              answers1.name,
+              answers1.id,
+              answers1.role,
+              answers2.email,
+            );
+            team.push(Employee);
+          }
+
+          if (answers1.role === "Intern") {
+            const intern = new Intern(
+              answers1.name,
+              answers1.id,
+              answers1.email,
+              answers1.role,
+              answers2.school
+            );
+            team.push(intern);
+          }
+
+          if (answers2.addMember) {
+            generateTeam();
+          } else {
+            team.forEach((team) => {
+              console.log(team);
+            });
+
+            fs.writeFile(outputPath, render(team), (err) => {
+              if (err) {
+                throw err;
+              }
+              console.log("Success, team HTML is created!");
+            });
+          }
+        });
+    })
+
+    .catch((err) => {
+      if (err) {
+        throw err;
+      }
     });
+};
+
+generateTeam();
